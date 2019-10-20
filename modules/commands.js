aee5c8db63079ff05dd7dacd9f078e1acfedf288
Skuru <skulkidd@gmail.com>
2019-10-20 14:34:17 +0200

    Cleaned up the code in TextCommands

--- modules/commands.js
+++ modules/commands.js
@@ -2,39 +2,17 @@ class TextCommand {
     constructor(name, work, chance){
       this.name = name;
       this.work = work;
-      if(chance == null){
-        this.chance = 100;
-      }
-      else{
-        this.chance = chance;
-      }
+      this.chance = chance || 100
     }
 
     generateAction(prefix, message, TextCommand){
-      	 if (message.content === (prefix + this.name) && !(message.author.bot) && !(message.content.includes("powiedz"))) {
+      	 if (message.content.toLowerCase() === (prefix + this.name.toLowerCase()) && !(message.author.bot) && !(message.content.includes("powiedz"))) {
         	message.channel.send(this.work);
       	}
     }
-    generateInclue(message){
-      var done = false
-       if (message.content.toLowerCase().includes(this.name) && !(message.author.bot) && !(message.content.includes("powiedz"))) {
-        message.channel.send(this.work);
-        done = true;
-      }
-      return done;
-    }
-    includeAndDelete(message){
+    generateInclude(message){
       var done = false;
-      if (message.content.toLowerCase().includes(this.name) && !(message.author.bot) && !(message.content.includes("powiedz"))) {
-       message.channel.send(this.work);
-       message.delete(0);
-       done = true;
-     }
-     return done;
-    }
-    generateRandom(message){
-      var done = false;
-      if (message.content.toLowerCase().includes(this.name) && !(message.author.bot) && !(message.content.includes("powiedz"))){
+      if (message.content.toLowerCase().includes(this.name.toLowerCase()) && !(message.author.bot) && !(message.content.includes("powiedz"))){
         if(Math.floor((Math.random() * 100) + 1) <= this.chance){
           message.channel.send(this.work);
           done = true;
@@ -45,4 +23,3 @@ class TextCommand {
 
 };
 module.exports = TextCommand;
-
